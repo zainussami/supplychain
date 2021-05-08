1e01f71ee59cc7a26b42a7ef5d7fcd314ffc1a00
Zain Ansari <zainussami@gmail.com>
2021-05-08 05:27:45 -0400

    First Commit

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,9 +1,23 @@
+
+const HDWallet = require('truffle-hdwallet-provider');
+const infuraKey = "d29d6ff907e7460b8c4d1db3e2453f72";
+//
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
-    }
+    },
+    rinkeby: {
+      provider: () => new HDWallet(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
+  }
   }
 };
---
+++ write-Up.md
@@ -0,0 +1,11 @@
+# Supply chain & data auditing
+Ethereum DApp that demonstrates a Supply Chain flow between a Seller and Buyer.
+
+## Versions
+Truffle v4.1.14 (core: 4.1.14)
+Solidity v0.4.24 (solc-js)
+node: '12.19.0'
+Web3 ^1.2.0
+
+## Transaction on Rinkeby Network 
+https://rinkeby.etherscan.io/address/0xce213450c84a21800672cc7985b2053f9b1096e5
